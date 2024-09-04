b60c44a5885dfa589430093b44cd96cbc087a99b
naphteine <58526116+naphteine@users.noreply.github.com>
2024-09-04 03:48:44 +0300

    First character

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex">
-      <h1>Unlimited Gokay Works</h1>
+    <main>
+      <h1>Fate/Grand Order</h1>
+      <p>Fate/Grand Order is a mobile game of Fate franchise.</p>
+
+      <h2>Characters</h2>
+      <p>There are characters in the game you can use to fight with. Here you can find information about some of them.</p>
+
+      <h3>Altria Pendragon</h3>
+      <ul>
+        <li><b>Class</b>: Saber</li>
+        <li>Illust: Takashi Takeuchi</li>
+        <li title="Character Voice">CV: Ayako Kawasumi</li>
+        <li>
+          Noble Phantasm
+          <ul>
+            <li>Name: Excalibur</li>
+            <li>Rank: A</li>
+            <li>Class: Anti-Fortress</li>
+          </ul>
+        </li>
+      </ul>
+
+      <footer>
+        This website (Unlimited) can be found on <a href="https://github.com/naphteine/unlimited">GitHub!</a><br/>
+        Fate/Grand Order is Copyright Aniplex Inc., DELiGHTWORKS, Aniplex of America and Sony Music Entertainment (Japan) Inc. All images and names owned and trademarked by Aniplex Inc., DELiGHTWORKS, Aniplex of America and Sony Music Entertainment (Japan) Inc. are property of their respective owners.
+      </footer>
     </main>
   );
 }
